c171fe3ba009bef183ba38ddf7fe0934de32a040
ket87 <ket8780@gmail.com>
2020-08-31 23:15:12 +0900

    Updates

---
+++ precache-manifest.25ccb11690bf22d0056b5e52f58d653d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622e15cf10566a5d4f2260f2a81bd2a2",
+    "url": "/Portfolio_React/index.html"
+  },
+  {
+    "revision": "da9be93df3e249f87540",
+    "url": "/Portfolio_React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5892642cae0cbe6c4fa1",
+    "url": "/Portfolio_React/static/js/2.923f68f6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio_React/static/js/2.923f68f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9be93df3e249f87540",
+    "url": "/Portfolio_React/static/js/main.7d425c8c.chunk.js"
+  },
+  {
+    "revision": "9402189c6674ad066739",
+    "url": "/Portfolio_React/static/js/runtime-main.a9e6076d.js"
+  },
+  {
+    "revision": "be16624f0285650d6ec95c3d1495ee84",
+    "url": "/Portfolio_React/static/media/portfolioPage.be16624f.PNG"
+  },
+  {
+    "revision": "8a2dd72543bcce0255bd8945501a19be",
+    "url": "/Portfolio_React/static/media/smartmirror.8a2dd725.jpg"
+  },
+  {
+    "revision": "076d78860cd41c41a29e56a644237d67",
+    "url": "/Portfolio_React/static/media/whatToEat.076d7886.jpg"
+  }
+]);
